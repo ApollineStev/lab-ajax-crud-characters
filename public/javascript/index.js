dfac43d3049c192b352729faa1f8b77f759e57ab
ApollineStev <apollinecanard@gmail.com>
2023-03-01 18:26:13 +0100

    iteration 1 and 2

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,29 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
+window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
+    // fetch all characters
+    document.querySelector("#fetch-all").addEventListener("click", async () => {
+      try {
+        const response = await charactersAPI.getFullList();
+        const allChars = response.data;
+        let cards = "";
+        allChars.forEach((char) => {
+          cards += `
+            <div class="character-info">
+                <div class="name">Name: ${char.name}</div>
+                <div class="occupation">Occupation: ${char.occupation}</div>
+                <div class="cartoon">Cartoon: ${char.cartoon ? "Yes" : "No"}</div>
+                <div class="weapon">Weapon: ${char.weapon}</div>
+            </div>
+            `;
+        });
+        document.querySelector(".characters-container").innerHTML = cards;
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
@@ -14,10 +34,12 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+                 
+    
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
   });
 });
+
